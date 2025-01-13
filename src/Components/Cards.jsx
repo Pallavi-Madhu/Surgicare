1b6d45537c8d7fba385d518d552d4d372bada77b
Kirubha V <vkirubha7@gmail.com>
2025-01-13 20:33:57 +0530

    About page completed

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = ({ image, description }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col items-center w-full max-w-xs">
+      <img
+        src={image}
+        className="object-contain w-full h-40 rounded-lg mb-4"
+        alt={description}
+      />
+      <div className="p-4 text-center">
+        <p className="text-gray-800 text-lg md:text-xl font-bold">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/Data/companyData.js
@@ -0,0 +1,51 @@
+// Company data array
+
+export const companies = [
+  {
+    name: "BL Lifesciences",
+    items: [
+      "BL LIFESCIENCES-CVTS Disposibles: Perfusion, Anesthesia & Surgery",
+      "ATRIVION-USA: OnX-Heart Valves and Bioglue",
+      "HELENA LABORATORIES-USA: ACT Machine and Tubes",
+      "XENIOS-GERMANY: ECMO Machine and Disposables",
+      "EUROSETS-GERMANY: Oxygenators",
+      "LSI SOLUTIONS-USA: COR-KNOT Device",
+      "SUN OPTIC-USA: LED & Xenon Headlights for Neuro and CVTS",
+      "SCANLAN INSTRUMENTS-USA: Micro-Surgery Instruments for CVTS & Neuro",
+      "Care Essentials Pty Ltd.-Australia: Cocoon Body Warmer & Blanket",
+      "DeSoutter Medical-UK: Sternum Saw",
+      "KINAMED-USA: Neuro Implants",
+      "Mizuho-Japan: Sugita Clips & Instruments",
+    ],
+  },
+  {
+    name: "Baxter India Pvt Ltd",
+    items: ["TISSEEL, FLOSEAL, HEMOPATCH, COSEAL, SEPRAFILM"],
+  },
+  {
+    name: "O&M Halyard Health India Pvt Ltd",
+    items: ["DRAPES, GOWNS & MASK"],
+  },
+  {
+    name: "B-BRAUN Medical India Ltd",
+    items: ["VASCULAR GRAFTS (SUB-DISTRIBUTOR)"],
+  },
+  {
+    name: "Insightra-Dolphin",
+    items: ["IABP BALLOONS"],
+  },
+  {
+    name: "Sinapi Biomedical - South Africa",
+    items: ["DRY SEAL CHEST DRAINAGE"],
+  },
+  {
+    name: "Toray",
+    items: ["HEEMOFEEL, TORAYMAXYN"],
+  },
+  {
+    name: "Terumo",
+    items: [
+      "Endovascular Harvesting System, HERC FLEX 360 ARM, TITAN FLEX 360 STABILIZER",
+    ],
+  },
+];
