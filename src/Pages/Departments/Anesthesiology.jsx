c92e83e6cc80e0b2ddf0651ad460a4d00bfb9be4
Pallavi-Madhu-X <pallavimadhu30@gmail.com>
2025-02-03 21:21:59 +0530

    uploaded images

--- src/Pages/Departments/Anesthesiology.jsx
+++ src/Pages/Departments/Anesthesiology.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import supabase from "../../config/contactClient";
 
 const Anesthesiology = () => {
-  return (
-    <div>Anesthesiology</div>
-  )
-}
+  const [images, setImages] = useState([]);
 
-export default Anesthesiology
+  useEffect(() => {
+    async function fetchImages() {
+      const { data, error } = await supabase
+        .from("departments")
+        .select("images");
+
+      if (error) {
+        console.error("error fetching images");
+      } else {
+        setImages(data);
+        console.log(data);
+      }
+    }
+    fetchImages();
+  }, []);
+
+  return (
+    <div>
+      <h1>helleo</h1>
+      <div className="">
+        {images.map((img, index) => {
+          return <img key={index} src={img.images} alt={`Image-${index}`} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Anesthesiology;
